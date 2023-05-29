50313b205ee0dadc650cb905d532d7e85ef5be38
JOY0987 <jjoyewon@gmail.com>
2023-05-29 14:11:22 +0900

    컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import SayHello from './SayHello';
+import ClickEvent from './component/chap01_event/ClickEvent';
+import ChangeEvent from './component/chap01_event/ChangeEvent';
+import SubmitEvent from './component/chap01_event/SubmitEvent';
 
 function App() {
+
+  const looping = () => {
+    const helloList = [];
+    for (let i = 0; i < 5; i++) {
+      helloList.push(<SayHello />);
+    }
+    return helloList;
+  };
+
+  // JSX 문법에서는 row한 스크립트 코드 사용 불가 (소괄호 안에서 불가)
+  // {} 안에서 함수호출문이나 변수 참조를 할 수 있음
   return (
-    <h1>리액트 초보입니다 ㅋㅋ</h1>
+    <>
+      <ChangeEvent />
+      <SubmitEvent />
+    </>
   );
 }
 
